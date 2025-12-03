d4ad76c7ce0f7f2204065aa0cef52abb3971360e
MutiaKusuma <mutiamharani@gmail.com>
2025-12-03 09:12:41 +0300

    Add project files.

---
+++ DenemeProject1/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DenemeProject1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DenemeProject1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Trainer> Trainers { get; set; }
+		public DbSet<TrainerSchedule> TrainerSchedules { get; set; }
+		public DbSet<Appointment> Appointments { get; set; }
+	}
+}
---
+++ DenemeProject1/Models/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DenemeProject1.Models
+{
+	public class Trainer
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public string Specialization { get; set; }
+
+		// Navigasi ke jadwal
+		public ICollection<TrainerSchedule> Schedules { get; set; }
+	}
+}
